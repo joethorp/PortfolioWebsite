7d24736baa5fc85866b58b30026df85b4976a5d6
Joe Thorp <80924843+joethorp@users.noreply.github.com>
2022-05-18 14:21:16 +0100

    second commit

---
+++ Desktop/G/Work/practiceone/FinalPortfolioWebsite/app.js
@@ -0,0 +1,9 @@
+const container = document.querySelector('.hero-container');
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach((btn) => {
+  btn.addEventListener('click', function () {
+    container.classList.toggle('active');
+  });
+});
+
